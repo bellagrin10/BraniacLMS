21eeb8431b029a8de809611915ffe6de54656b7f
bellagrin10 <bellgrinberg@gmail.com>
2022-05-05 19:50:00 +0300

    lesson_3

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,7 +1,8 @@
 #from django.shortcuts import render
 
+from black import Any, Dict
 from django.views.generic import TemplateView
-
+from datetime import datetime
 
 class MainPageView(TemplateView):
     template_name = 'mainapp/index.html'
@@ -9,6 +10,16 @@ class MainPageView(TemplateView):
 
 class NewsPageView(TemplateView):
     template_name = 'mainapp/news.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context = {
+            'loop_times': range(1, 6),
+            'title': 'Новостной заголовок',
+            'preview': 'Предварительное описание',
+            'date': datetime.now()
+        }
+        return context
 
 class CoursesPageView(TemplateView):
     template_name = 'mainapp/courses_list.html'
