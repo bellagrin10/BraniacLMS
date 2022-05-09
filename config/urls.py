1d719e3d4772b506a9f7bb922319d91377abf9b4
bellagrin10 <bellgrinberg@gmail.com>
2022-05-09 03:06:09 +0300

    changes added

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', RedirectView.as_view(url='mainapp/')),
     path('mainapp/', include('mainapp.urls', namespace='mainapp')),
+    path('mainapp/news/<term>/',
+         RedirectView.as_view(url='https://google.com/?q=%(term)s')),
 ]
